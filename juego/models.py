b05f9f58f9031a2ad48554b9432a2a3b8048157f
CurroAyala <curroayala18@gmail.com>
2026-05-17 21:16:24 +0200

    Template de inicio añadida

--- juego/models.py
+++ juego/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Matricula(models.Model):
+    letras = models.CharField(max_length=3)
+    respuesta = models.CharField(max_length=100)
+
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name='matriculas')
+
+    def __str__(self):
+        return f"{self.letras} -> {self.respuesta} ({self.usuario.username})"
--- juego/views.py
+++ juego/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def inicio(request):
+    """
+    Esta vista se encarga de renderizar la página de inicio.
+    """
+    return render(request, 'juego/inicio.html')
